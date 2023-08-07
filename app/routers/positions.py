3bad1de867c7c30b3b70c1ac754fa59f67d19c6b
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-08-07 15:46:30 +0330

    pref: add position api

--- app/routers/positions.py
+++ app/routers/positions.py
@@ -6,10 +6,10 @@ from app.database import engine, Position
 router = APIRouter()
 
 
-@router.get("/position/{id}", response_model=Position, tags=["position"])
-async def retrieve_one_position(id: int):
+@router.get("/position/{position_id}", response_model=Position, tags=["position"])
+async def retrieve_one_position(position_id: int):
     with Session(engine) as session:
-        position = session.get(Position, id)
+        position = session.get(Position, position_id)
         if not position:
             raise HTTPException(status_code=404, detail="Not found")
         return position
