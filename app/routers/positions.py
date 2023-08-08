698894c8edbf54088dc26cf58abe3e4c4b620277
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-08-08 13:50:12 +0330

    ci: init github action

--- app/routers/positions.py
+++ app/routers/positions.py
@@ -11,5 +11,5 @@ async def retrieve_one_position(position_id: int):
     with Session(engine) as session:
         position = session.get(Position, position_id)
         if not position:
-            raise HTTPException(status_code=404, detail="Not found")
+            raise HTTPException(status_code=404, detail="Position not found")
         return position
