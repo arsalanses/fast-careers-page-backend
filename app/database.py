56b833c3d18a28e8a15ea2185ad2462eb8fe855f
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2023-08-09 23:22:57 +0330

    pref: revert

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine, select, Session
 
 from .models import Department, Application, Position
 
-sqlite_file_name = "database/database.db"
+sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
