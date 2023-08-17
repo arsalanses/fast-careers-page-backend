d29e265f2baa52edf525c6e9324ca5922433e795
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-08-17 13:42:10 +0330

    pref: add jinja tmpl dep

--- app/routers/applications.py
+++ app/routers/applications.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 
 @router.post("/applications", response_model=Application, tags=["application"])
-def create_application(application: Application, tags=["application"]):
+async def create_application(application: Application):
     # TODO: Read about MultiClass models
     application.is_approved = None
     
@@ -16,3 +16,20 @@ def create_application(application: Application, tags=["application"]):
         session.commit()
         session.refresh(application)
         return application
+
+# from fastapi import APIRouter, UploadFile, File
+# from typing import Union
+# from sqlmodel.ext.asyncio.session import AsyncSession
+# @router.post("/applications", tags=["application"]) # response_model=Application
+# async def create_application(application: Application, file: Union[UploadFile, None] = None):
+#     application.is_approved = None
+
+#     contents = await file.read()
+    
+#     async with AsyncSession(engine) as session:
+#         async with session.begin():
+#             session.add(application)
+#             await session.commit()
+#             await session.refresh(application)
+
+#     return application
