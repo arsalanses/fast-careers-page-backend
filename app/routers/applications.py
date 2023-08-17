be991d886d11fc7ca5348e04f9ad7b702ffd1b5b
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-08-17 19:07:27 +0330

    pref: add form

--- app/routers/applications.py
+++ app/routers/applications.py
@@ -1,21 +1,34 @@
-from fastapi import APIRouter
-
-from sqlmodel import Session
 from app.database import engine, Application
+from fastapi import APIRouter, Form, Header
+from fastapi.responses import HTMLResponse
+from sqlmodel import Session
 
 router = APIRouter()
 
 
-@router.post("/applications", response_model=Application, tags=["application"])
-async def create_application(application: Application):
-    # TODO: Read about MultiClass models
-    application.is_approved = None
-    
+@router.post("/applications", tags=["application"])
+async def create_application(jobseeker_name: str = Form(),
+                             jobseeker_phone: str = Form(),
+                             jobseeker_mail: str = Form(),
+                             jobseeker_resume: str = Form(),
+                             position_id: str = Form(),
+                             accept: str = Form()):
+
+    application = Application(jobseeker_name=jobseeker_name,
+                              jobseeker_phone=jobseeker_phone,
+                              jobseeker_mail=jobseeker_mail,
+                              jobseeker_resume=jobseeker_resume,
+                              position_id=position_id)
+
     with Session(engine) as session:
         session.add(application)
         session.commit()
         session.refresh(application)
-        return application
+    
+    if accept == "text/html":
+        return HTMLResponse(content="<b>Application submitted</b>", status_code=200)
+    
+    return application
 
 # from fastapi import APIRouter, UploadFile, File
 # from typing import Union
