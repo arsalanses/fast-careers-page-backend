db266573c914df477a5fbc711a58fe5c59e5714b
arsalanse <dr.sefidgar@gmail.com>
2023-08-18 00:29:11 +0330

    pref: file upload

--- app/routers/applications.py
+++ app/routers/applications.py
@@ -1,23 +1,23 @@
 from app.database import engine, Application
-from fastapi import APIRouter, Form, Header
+from fastapi import APIRouter, Form, UploadFile
 from fastapi.responses import HTMLResponse
 from sqlmodel import Session
+from typing import Annotated
 
 router = APIRouter()
 
-
 @router.post("/applications", tags=["application"])
-async def create_application(jobseeker_name: str = Form(),
-                             jobseeker_phone: str = Form(),
-                             jobseeker_mail: str = Form(),
-                             jobseeker_resume: str = Form(),
+async def create_application(jobseeker_name: Annotated[str, Form()],
+                             jobseeker_phone: Annotated[str, Form()],
+                             jobseeker_mail: Annotated[str, Form()],
+                             jobseeker_resume: UploadFile | None = None,
                              position_id: str = Form(),
                              accept: str = Form()):
 
     application = Application(jobseeker_name=jobseeker_name,
                               jobseeker_phone=jobseeker_phone,
                               jobseeker_mail=jobseeker_mail,
-                              jobseeker_resume=jobseeker_resume,
+                              jobseeker_resume=jobseeker_resume.filename,
                               position_id=position_id)
 
     with Session(engine) as session:
@@ -29,20 +29,3 @@ async def create_application(jobseeker_name: str = Form(),
         return HTMLResponse(content="<b>Application submitted</b>", status_code=200)
     
     return application
-
-# from fastapi import APIRouter, UploadFile, File
-# from typing import Union
-# from sqlmodel.ext.asyncio.session import AsyncSession
-# @router.post("/applications", tags=["application"]) # response_model=Application
-# async def create_application(application: Application, file: Union[UploadFile, None] = None):
-#     application.is_approved = None
-
-#     contents = await file.read()
-    
-#     async with AsyncSession(engine) as session:
-#         async with session.begin():
-#             session.add(application)
-#             await session.commit()
-#             await session.refresh(application)
-
-#     return application
