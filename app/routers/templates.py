5a18d4818a02774aa02818aa7b962a729e225ed9
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-08-17 17:00:15 +0330

    pref: htmx initial

--- app/routers/templates.py
+++ app/routers/templates.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
+
+from sqlmodel import select, Session
+from app.database import engine, Department, Position
 
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -9,4 +12,16 @@ router = APIRouter()
 
 @router.get("/home", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    with Session(engine) as session:
+        statement = select(Department)
+        results = session.exec(statement)
+        department_list = [[department, department.positions] for department in results]
+    return templates.TemplateResponse("home.html", {"request": request, "results": department_list})
+
+@router.get("/home/position/{position_id}", response_class=HTMLResponse)
+async def home(request: Request, position_id: int):
+    with Session(engine) as session:
+        position = session.get(Position, position_id)
+        if not position:
+            raise HTTPException(status_code=404, detail="Position not found")
+        return templates.TemplateResponse("position.html", {"request": request, "results": position})
