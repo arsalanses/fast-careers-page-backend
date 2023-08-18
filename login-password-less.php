5b18f25c191526eb3ae34bbc5d183faace562291
arsalanse <dr.sefidgar@gmail.com>
2023-08-18 15:20:44 +0330

    pref: adminer

--- login-password-less.php
+++ login-password-less.php
@@ -1,6 +1,8 @@
 <?php
-    require_once('plugins/login-password-less.php');
-    /** Set allowed password
-     * @param string result of password_hash
-     */
-    return new AdminerLoginPasswordLess($password_hash = 'admin');
+require_once('plugins/login-password-less.php');    
+/** Set allowed password
+ * @param string result of password_hash
+ */
+return new AdminerLoginPasswordLess(
+    $password_hash = password_hash("admin", PASSWORD_DEFAULT)
+);
